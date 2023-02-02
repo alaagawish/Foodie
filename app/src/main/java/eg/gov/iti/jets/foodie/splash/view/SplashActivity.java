9f9b1c1a25fe79ddea5f831474a18bd4bea134da
alaagawish <53160167+alaagawish@users.noreply.github.com>
2023-02-02 12:12:23 +0200

    Splash screen

--- app/src/main/java/eg/gov/iti/jets/foodie/splash/view/SplashActivity.java
+++ app/src/main/java/eg/gov/iti/jets/foodie/splash/view/SplashActivity.java
@@ -2,14 +2,31 @@ package eg.gov.iti.jets.foodie.splash.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import eg.gov.iti.jets.foodie.MainActivity;
 import eg.gov.iti.jets.foodie.R;
 
 public class SplashActivity extends AppCompatActivity {
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        lottieAnimationView = findViewById(R.id.lottieLogo);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 5000);
     }
 }
