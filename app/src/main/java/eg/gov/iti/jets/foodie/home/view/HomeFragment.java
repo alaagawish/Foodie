f5ca6cd46f4a1ad1562513833e186817b86b6e19
ahd <ahdwael28@gmail.com>
2023-02-05 01:52:28 +0200

    adding slider Adapter

--- app/src/main/java/eg/gov/iti/jets/foodie/home/view/HomeFragment.java
+++ app/src/main/java/eg/gov/iti/jets/foodie/home/view/HomeFragment.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import eg.gov.iti.jets.foodie.home.view.Slider;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import eg.gov.iti.jets.foodie.R;
 
@@ -19,6 +25,11 @@ public class HomeFragment extends Fragment {
     Button categoryButton;
     Button ingredientButton;
     Button regionButton;
+    SliderView sliderView;
+    SliderAdapter sliderAdapter;
+
+    private ArrayList<Slider> sliderArrayList;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,17 @@ public class HomeFragment extends Fragment {
         categoryButton = view.findViewById(R.id.categoryButton);
         ingredientButton = view.findViewById(R.id.ingredientButton);
         regionButton = view.findViewById(R.id.regionButton);
+        sliderView = view.findViewById(R.id.imageSlider);
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+        sliderArrayList = new ArrayList<>();
+        sliderArrayList.add(new Slider("https://th.bing.com/th/id/R.4296f49fb527bdaf3039e31e1d0dcb2f?rik=0NrAyeJdAkOzwQ&pid=ImgRaw&r=0"));
+        sliderArrayList.add(new Slider("https://westernnews.media.clients.ellingtoncms.com/img/photos/2018/02/09/Pancakes.jpeg"));
+        sliderArrayList.add(new Slider("https://www.inliv.com/wp-content/uploads/2017/06/Untitled-design-e1530748897399.jpg"));
+        sliderAdapter = new SliderAdapter(sliderArrayList);
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setScrollTimeInSec(3);
+        sliderView.setAutoCycle(true);
+        sliderView.startAutoCycle();
 
         categoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/eg/gov/iti/jets/foodie/home/view/Slider.java
@@ -0,0 +1,18 @@
+package eg.gov.iti.jets.foodie.home.view;
+
+public class Slider {
+
+    private String imgUrl;
+
+    public Slider(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
