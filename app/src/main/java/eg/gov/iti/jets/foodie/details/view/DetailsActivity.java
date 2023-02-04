3c90bda32899825b344229cfb2e9bb3d239030c1
alaagawish <53160167+alaagawish@users.noreply.github.com>
2023-02-04 19:48:43 +0200

    meal details activity design

--- app/src/main/java/eg/gov/iti/jets/foodie/details/view/DetailsActivity.java
+++ app/src/main/java/eg/gov/iti/jets/foodie/details/view/DetailsActivity.java
@@ -1,14 +1,39 @@
 package eg.gov.iti.jets.foodie.details.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
 import eg.gov.iti.jets.foodie.R;
+
 public class DetailsActivity extends AppCompatActivity {
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        youTubePlayerView = findViewById(R.id.youtubePlayerView);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "1FJHYqE0RDg";
+//                youTubePlayer.loadVideo(videoId, 0);
+                youTubePlayer.cueVideo(videoId, 0);
+            }
+        });
+    }
+
+    public void AddFav(View view) {
+
     }
 }
