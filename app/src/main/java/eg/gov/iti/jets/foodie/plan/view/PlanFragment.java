6e5ccc6acd340aa0aff2a3d5c45db62e46bd3d01
alaagawish <53160167+alaagawish@users.noreply.github.com>
2023-02-05 15:33:46 +0200

    dialog

--- app/src/main/java/eg/gov/iti/jets/foodie/plan/view/PlanFragment.java
+++ app/src/main/java/eg/gov/iti/jets/foodie/plan/view/PlanFragment.java
@@ -1,5 +1,6 @@
 package eg.gov.iti.jets.foodie.plan.view;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,9 +8,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +28,12 @@ import eg.gov.iti.jets.foodie.model.Meal;
 
 
 public class PlanFragment extends Fragment implements AllMealsClickListener {
-
-    RecyclerView saturdayRecyclerView, sundayRecyclerView, mondayRecyclerView, tuesdayRecyclerView, wednesdayRecyclerView, thursdayRecyclerView, fridayRecyclerView;
-    DayAdapter dayAdapter;
-    List<Meal> meals;
+    private static final String TAG = "PlanFragment";
+    private RecyclerView saturdayRecyclerView, sundayRecyclerView, mondayRecyclerView, tuesdayRecyclerView, wednesdayRecyclerView, thursdayRecyclerView, fridayRecyclerView;
+    private DayAdapter dayAdapter;
+    private List<Meal> meals;
+    private Dialog searchDialog;
+    private MaterialButton saturdayAddMaterialButton, sundayAddMaterialButton, mondayAddMaterialButton, tuesdayAddMaterialButton, thursdayAddMaterialButton, wednesdayAddMaterialButton, fridayAddMaterialButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,13 +53,8 @@ public class PlanFragment extends Fragment implements AllMealsClickListener {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         dumy();
-        saturdayRecyclerView = view.findViewById(R.id.saturdayRecyclerView);
-        sundayRecyclerView = view.findViewById(R.id.sundayRecyclerView);
-        mondayRecyclerView = view.findViewById(R.id.mondayRecyclerView);
-        tuesdayRecyclerView = view.findViewById(R.id.tuesdayRecyclerView);
-        wednesdayRecyclerView = view.findViewById(R.id.wednesdayRecyclerView);
-        thursdayRecyclerView = view.findViewById(R.id.thursdayRecyclerView);
-        fridayRecyclerView = view.findViewById(R.id.fridayRecyclerView);
+        init(view);
+
         dayAdapter = new DayAdapter(getContext(), PlanFragment.this);
         dayAdapter.setAllMeals(meals);
         saturdayRecyclerView.setAdapter(dayAdapter);
@@ -59,6 +65,14 @@ public class PlanFragment extends Fragment implements AllMealsClickListener {
         thursdayRecyclerView.setAdapter(dayAdapter);
         fridayRecyclerView.setAdapter(dayAdapter);
         dayAdapter.notifyDataSetChanged();
+
+        saturdayAddMaterialButton.setOnClickListener(v -> {
+            showDialog();
+        });
+        sundayAddMaterialButton.setOnClickListener(v -> {
+            showDialog();
+        });
+
     }
 
     public void dumy() {
@@ -68,5 +82,45 @@ public class PlanFragment extends Fragment implements AllMealsClickListener {
         meals.add(new Meal("Meat", "https://th.bing.com/th/id/R.a86a695d7575310d4af66450ffe8ce1d?rik=7%2f4%2fov%2fN4ecSzQ&pid=ImgRaw&r=0"));
         meals.add(new Meal("Meat", "https://th.bing.com/th/id/R.a86a695d7575310d4af66450ffe8ce1d?rik=7%2f4%2fov%2fN4ecSzQ&pid=ImgRaw&r=0"));
         meals.add(new Meal("Meat", "https://th.bing.com/th/id/R.a86a695d7575310d4af66450ffe8ce1d?rik=7%2f4%2fov%2fN4ecSzQ&pid=ImgRaw&r=0"));
+    }
+
+    public void showDialog() {
+        Log.d(TAG, "showDialog: ddddddddd");
+        View dialogLayout = LayoutInflater.from(getContext()).inflate(R.layout.dialog_search, null);
+        searchDialog = new Dialog(getContext());
+        searchDialog.setContentView(dialogLayout);
+        searchDialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        searchDialog.show();
+        searchDialog.findViewById(R.id.searchDialogEditText).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText searchDialogEditText = searchDialog.findViewById(R.id.searchDialogEditText);
+                String searchText = searchDialogEditText.getText().toString().trim();
+                if (searchText.isEmpty()) {
+                    searchDialogEditText.setError("Enter a meal");
+                } else {
+//                    searchDialog.findViewById(R.id.searchResultRecyclerView).setVisibility(View.VISIBLE);
+//                    searchDialog.dismiss();
+                }
+            }
+
+        });
+    }
+
+    public void init(View view) {
+        saturdayRecyclerView = view.findViewById(R.id.saturdayRecyclerView);
+        sundayRecyclerView = view.findViewById(R.id.sundayRecyclerView);
+        mondayRecyclerView = view.findViewById(R.id.mondayRecyclerView);
+        tuesdayRecyclerView = view.findViewById(R.id.tuesdayRecyclerView);
+        wednesdayRecyclerView = view.findViewById(R.id.wednesdayRecyclerView);
+        thursdayRecyclerView = view.findViewById(R.id.thursdayRecyclerView);
+        fridayRecyclerView = view.findViewById(R.id.fridayRecyclerView);
+        saturdayAddMaterialButton = view.findViewById(R.id.saturdayAddMaterialButton);
+        sundayAddMaterialButton = view.findViewById(R.id.sundayAddMaterialButton);
+        mondayAddMaterialButton = view.findViewById(R.id.mondayAddMaterialButton);
+        tuesdayAddMaterialButton = view.findViewById(R.id.tuesdayAddMaterialButton);
+        wednesdayAddMaterialButton = view.findViewById(R.id.wednesdayAddMaterialButton);
+        thursdayAddMaterialButton = view.findViewById(R.id.thursdayAddMaterialButton);
+        fridayAddMaterialButton = view.findViewById(R.id.fridayAddMaterialButton);
     }
 }
