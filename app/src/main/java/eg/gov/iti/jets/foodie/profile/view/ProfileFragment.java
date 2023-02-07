32fd36322b651999637788c8003b3658fc818478
ahd <ahdwael28@gmail.com>
2023-02-07 02:56:08 +0200

    Login with Google and SignOut using Firebase

--- app/src/main/java/eg/gov/iti/jets/foodie/profile/view/ProfileFragment.java
+++ app/src/main/java/eg/gov/iti/jets/foodie/profile/view/ProfileFragment.java
@@ -11,6 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import eg.gov.iti.jets.foodie.MainActivity;
 import eg.gov.iti.jets.foodie.R;
@@ -18,7 +27,11 @@ import eg.gov.iti.jets.foodie.login.view.LoginActivity;
 
 public class ProfileFragment extends Fragment {
 
+    TextView usernameTextView;
     Button logoutButton;
+    FirebaseAuth mAuth;
+    GoogleSignInClient gsc;
+    GoogleSignInOptions gso;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +47,30 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        usernameTextView = view.findViewById(R.id.usernameTextView);
         logoutButton = view.findViewById(R.id.logoutButton);
+        mAuth = FirebaseAuth.getInstance();
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(getContext(),gso);
+
+        GoogleSignInAccount account=GoogleSignIn.getLastSignedInAccount(getContext());
+        if(account != null)
+        {
+            String userName = account.getDisplayName();
+            usernameTextView.setText(userName);
+        }
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
+                gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
             }
         });
     }
