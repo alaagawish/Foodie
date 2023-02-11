f0db2d7c828e705713df0be8871e29b289a0bf7c
ahd <ahdwael28@gmail.com>
2023-02-11 19:15:08 +0200

    handling caching

--- app/src/main/java/eg/gov/iti/jets/foodie/MainActivity.java
+++ app/src/main/java/eg/gov/iti/jets/foodie/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.File;
+
 import eg.gov.iti.jets.foodie.favourites.view.FavouritesFragment;
 import eg.gov.iti.jets.foodie.home.view.HomeFragment;
 import eg.gov.iti.jets.foodie.login.view.LoginActivity;
@@ -24,18 +26,18 @@ import eg.gov.iti.jets.foodie.search.view.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
-    private HomeFragment homeFragment = new HomeFragment();
-    private ProfileFragment profileFragment = new ProfileFragment();
-    private FavouritesFragment favouritesFragment = new FavouritesFragment();
-    private SearchFragment searchFragment = new SearchFragment();
-    private PlanFragment planFragment = new PlanFragment();
+    private HomeFragment homeFragment;
+    private ProfileFragment profileFragment;
+    private FavouritesFragment favouritesFragment;
+    private SearchFragment searchFragment;
+    private PlanFragment planFragment;
+    public static File cacheFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        init();
         getSupportFragmentManager().beginTransaction().replace(R.id.containerFrameLayout, homeFragment).commit();
         SharedPreferences sharedPreferences1 = getSharedPreferences(LoginActivity.PREF, MODE_PRIVATE);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -48,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     case R.id.profile:
                         if (sharedPreferences1.getString(LoginActivity.EMAIL, "NOT FOUND").equals("NOT FOUND")) {
-                           dialog();
+                            dialog();
                         } else {
                             getSupportFragmentManager().beginTransaction().replace(R.id.containerFrameLayout, profileFragment).commit();
                         }
@@ -70,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
                             getSupportFragmentManager().beginTransaction().replace(R.id.containerFrameLayout, favouritesFragment).commit();
                         }
                         return true;
+
+
                 }
                 return false;
             }
@@ -80,12 +84,23 @@ public class MainActivity extends AppCompatActivity {
     public void back(View view) {
         finish();
     }
-    public void dialog()
-    {
+
+    public void dialog() {
         View dialogLayout = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_guest, null);
         HomeFragment.searchDialog = new Dialog(MainActivity.this);
         HomeFragment.searchDialog.setContentView(dialogLayout);
         HomeFragment.searchDialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         HomeFragment.searchDialog.show();
+    }
+
+    public void init() {
+        cacheFile = MainActivity.this.getCacheDir();
+        planFragment = new PlanFragment();
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+        favouritesFragment = new FavouritesFragment();
+        searchFragment = new SearchFragment();
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+
     }
 }
